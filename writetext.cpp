943f815dddd5b8c74dd0d1a4a856ecd49ed0fc8b
AlexWonder <rich.stp.p@gmail.com>
2017-04-05 11:48:17 -0400

    Fun with text files

--- writetext.cpp
+++ writetext.cpp
@@ -9,7 +9,7 @@ int main( )
    char c = 'J';
 
    ofstream myfile{"abc.txt"};
-   if (myfile) //check if opened successfully
+   if (myfile) //check if file opened successfully
    	myfile<<a<<" "<<b<<" "<<c;
    else
 	cout << "Error opening abc.txt" << endl;
